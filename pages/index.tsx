e7ed445ede765a24d11a7cf02704ee6371e76170
dvdapogeo <dvdingles@hotmail.com>
2022-09-20 13:58:54 +0200

    reescalo

--- pages/index.tsx
+++ pages/index.tsx
@@ -63,7 +63,7 @@ const Home: NextPage = () => {
           
             <Dav 
               // className={styles.dav}
-              scale={.7}
+              scale={.5}
               position={[0,0.15,0]}
               rotation={[Math.PI / 1000, 0, 0]}
             />
